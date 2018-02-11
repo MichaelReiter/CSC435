bc4c6941386b1bf59e0565eb8cc16c6e7b4b5db3
Michael Reiter <michaelreiter@live.ca>
2018-02-10 18:50:12 -0800

    Add line and offset getters/setters to ASTNode

--- src/ast/ASTNode.java
+++ src/ast/ASTNode.java
@@ -7,4 +7,20 @@ public abstract class ASTNode {
     private int offset;
 
     public abstract Type accept(Visitor v);
+
+    public final int getLine() {
+        return this.line;
+    }
+
+    public final int getOffset() {
+        return this.offset;
+    }
+
+    public final void setLine(int line) {
+        this.line = line;
+    }
+
+    public final void setOffset(int offset) {
+        this.offset = offset;
+    }
 }
