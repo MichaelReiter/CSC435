fc0db7b59a9523fe8923df2372d9edd3cf6fd872
Michael Reiter <michaelreiter@live.ca>
2018-03-03 11:26:32 -0800

    Add ReturnInstruction

---
+++ src/ir/ReturnInstruction.java
@@ -0,0 +1,17 @@
+package ir;
+
+public class ReturnInstruction extends Instruction {
+    private final Temp temp;
+
+    public ReturnInstruction(Temp temp) {
+        this.temp = temp;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("RETURN ");
+        sb.append(this.temp);
+        sb.append(";");
+        return sb.toString();
+    }
+}
