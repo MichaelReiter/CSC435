2aed64351cc9aa705b848701ffa060fd8c749830
Michael Reiter <michaelreiter@live.ca>
2018-03-04 19:07:08 -0800

    Rename AssignmentOperation to AssignmentInstruction

---
+++ src/ir/AssignmentInstruction.java
@@ -0,0 +1,23 @@
+package ir;
+
+import java.lang.StringBuilder;
+
+public class AssignmentInstruction extends Instruction {
+    private final Operand operand1;
+    private final Operand operand2;
+
+    public AssignmentInstruction(Operand operand1, Operand operand2) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.operand1);
+        sb.append(" := ");
+        sb.append(this.operand2);
+        sb.append(";");
+        return sb.toString();
+    }
+}
