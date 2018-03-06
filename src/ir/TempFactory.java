00224fd7b80c0f2251ba743608d449b12af95cfd
Michael Reiter <michaelreiter@live.ca>
2018-03-06 13:16:05 -0800

    Refactor TempFactory and add printing

--- src/ir/TempFactory.java
+++ src/ir/TempFactory.java
@@ -2,21 +2,38 @@ package ir;
 
 import type.Type;
 import ir.Temp.TempClass;
+import java.lang.StringBuilder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TempFactory {
-    private int count;
+    private List<Temp> temps;
 
     public TempFactory() {
-        this.count = 0;
+        this.temps = new ArrayList<Temp>();
     }
 
-    public Temp getTemp(Type type) {
-        Temp temp = new Temp(type, this.count);
-        this.count++;
+    public Temp getTemp(Type type, TempClass tempClass) {
+        Temp temp = new Temp(type, this.temps.size(), tempClass);
+        this.temps.add(temp);
         return temp;
     }
 
-    public Temp getTemp(Type type, TempClass tempClass, String name) {
-        return null;
+    // public Temp getTemp(Type type, TempClass tempClass, String name) {
+    //     return null;
+    // }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Temp t : this.temps) {
+            sb.append("    TEMP ");
+            sb.append(t.getNumber());
+            sb.append(":");
+            sb.append(t.getType().toChar());
+            sb.append(";\n");
+        }
+        sb.append("\n");
+        return sb.toString();
     }
 }
